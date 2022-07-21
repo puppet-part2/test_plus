0593ca09ed20d54f0b2da1baa7c2394b3ad6f1e5
puppet-part2 <2578114188@qq.com>
2022-07-21 15:07:24 +0800

    Update afl-fuzz.c

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -21783,11 +21783,13 @@ int main(int argc, char **argv)
       ck_free(bb_new);
     }
     //new code end
+    /*
     SAYF("\n\n[ ");
     for(int kkwra = 0; kkwra < constant_array_max; kkwra++){
       SAYF("%u, ",constant_array[kkwra]);
     }
     SAYF(" ]");
+    */
 
     //new code end
 
